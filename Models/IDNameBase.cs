672e67b6b667254e9aaade2a7f42e5515be3370f
nathnado22 <nadocouniversal@gmail.com>
2020-07-16 13:30:33 +1000

    Initial Commit

---
+++ Models/IDNameBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASP_MyBSNList.Models
+{
+    public class IDNameBase
+    {
+        byte Id { get; set; }
+
+        [Required]
+        string Name { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP_MyBSNList.Startup))]
+namespace ASP_MyBSNList
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
